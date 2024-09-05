5754d865c2829870429bbbd9f9596cda9d66fef5
tearsqwe <chernyakpavel44@gmail.com>
2024-09-05 19:53:22 +0300

    added all activities

--- app/src/main/java/com/example/gittest/DeleteActivity.kt
+++ app/src/main/java/com/example/gittest/DeleteActivity.kt
@@ -1,25 +1,16 @@
 package com.example.gittest
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class DeleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val btnCreate: Button= findViewById(R.id.button)
-        val btnRead: Button= findViewById(R.id.button2)
-        val btnUpdate: Button= findViewById(R.id.button3)
-        val btnDelete: Button= findViewById(R.id.button4)
-
-
-
+        setContentView(R.layout.activity_delete)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
